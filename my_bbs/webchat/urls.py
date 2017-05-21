0b2100090108bcd8cc4b134ba9ba53ce642cc21d
0zcl <1274828542@qq.com>
2017-05-22 01:01:21 +0800

    today is a hard day,I continue to finish my webchat home;I let it show a little beautiful thought i still have a long way to go;Attention, you can see what i do today from a picture(6.png);what's more,web_server can reveive message from web by ajax.

--- my_bbs/webchat/urls.py
+++ my_bbs/webchat/urls.py
@@ -3,5 +3,5 @@ from webchat import views
 
 urlpatterns = [
     url(r'^$', views.chat_dashboard),
-
+    url(r'^msg_send/$', views.send_msg, name="send_msg"),  # url不写动词
 ]
--- my_bbs/webchat/views.py
+++ my_bbs/webchat/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse,HttpResponseRedirect
 from bbs import models
 # Create your views here.
 
@@ -7,3 +7,8 @@ def chat_dashboard(request):
     category_list = models.Category.objects.filter(set_to_top_menu=True).order_by("position_index")
 
     return render(request, "webchat/chat_index.html", {"category_list": category_list})
+
+def send_msg(request):
+    print(request.POST)
+    print(">>接收到的消息", request.POST.get("msg"))
+    return HttpResponse("---receive msg---")
