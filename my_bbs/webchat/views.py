6eedaea55f1fa37399159b570e78f0aab6d2f49e
0zcl <1274828542@qq.com>
2017-05-24 21:58:03 +0800

    I will write a blog tonight to record the problem I met this days.yesterday i met some bug,and i write some JS functions cause my mind not so clearly, so this day i record every step i do in my onenote book,luckily i did solve my bug tonight.

--- my_bbs/webchat/views.py
+++ my_bbs/webchat/views.py
@@ -17,18 +17,19 @@ def chat_dashboard(request):
 def send_msg(request):
     print(request.POST)
     msg_dic = request.POST.get("msg_dic")
-    print(msg_dic, type(msg_dic))  # eg:{"from":"3","to":"2","type":"single","msg":"1111"}
+    print(msg_dic, type(msg_dic))  # eg:{"from":"3","to":"2","type":"single","msg":"1111"} <class 'str'>
     if msg_dic:
         msg_dic = json.loads(msg_dic)
-        print(msg_dic, type(msg_dic))
-        print(">>接收到的消息", msg_dic["msg"])
+        print(msg_dic, type(msg_dic))  # {'type': 'single', 'from': '3', 'to': '1', 'msg': '3333'} <class 'dict'>
+        print(">>接收到的消息", msg_dic["msg"])  # >>接收到的消息 3333
         queue_id = int(msg_dic["to"])  # 接收消息的用户id
         msg_dic["timestamp"] = time.time()  # 添加时间
         if msg_dic["type"] == "single":  # 一对一发送
-            if not GLOBAL_MSG_QUEUES.get(queue_id):  # 如果用户队列不存在
+            # 如果用户队列不存在, 注意，如果id(key)对应的队列不存在，则输出None,不会曝错
+            if not GLOBAL_MSG_QUEUES.get(queue_id):
                 GLOBAL_MSG_QUEUES[queue_id] = queue.Queue()  # 创建队列
 
-            GLOBAL_MSG_QUEUES[queue_id].put(msg_dic["msg"])  # 将消息放进队列
+            GLOBAL_MSG_QUEUES[queue_id].put(msg_dic)  # 将消息字典(带时间戳)放进队列
             print("全局队列>>:", GLOBAL_MSG_QUEUES)
 
     return HttpResponse("---receive msg---")
@@ -41,12 +42,12 @@ def get_new_msgs(request):  # 用户接收消息
         GLOBAL_MSG_QUEUES[request.user.userprofile.id] = queue.Queue()  # 创建队列
 
     q_obj = GLOBAL_MSG_QUEUES[request.user.userprofile.id]  # 队列对象
-    msg_size = q_obj.qsize()  # 队列长度
+    msg_size = q_obj.qsize()  # 队列长度（即消息数量）
     msg_list = []
     if msg_size > 0:  # 有消息
         for msg in range(msg_size):
-            msg_list.append(q_obj.get())  # 从队列取消息(先入先出)
-        print("new msg:", msg_list)
+            msg_list.append(q_obj.get())  # 从队列取消息，即数据字典(先入先出)
+        print(">>new msg:", msg_list)
     else:  # 没消息，要挂起
         # 超时挂起
         # 若队列为空，则60秒后会曝queue.Empty异常(相当在这60秒内卡住挂起)
@@ -54,6 +55,7 @@ def get_new_msgs(request):  # 用户接收消息
             msg_list.append(q_obj.get(timeout=60))
         except queue.Empty:  # 若队列为空，则60秒后会曝queue.Empty异常(相当在这60秒内卡住挂起)
             print("\033[41;1mno msg for [%s][%s],timeout\033[0m" % (request.user.userprofile.id,request.user))
+    print(">>>msg_list", msg_list)  # msg_list为消息列表，存放未接收的消息字典
     return HttpResponse(json.dumps(msg_list))  # 序列化，转化为json格式
 
 
