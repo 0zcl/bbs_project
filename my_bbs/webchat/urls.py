5b9d1c84316af069b1a12e87a622eaa012efad79
0zcl <1274828542@qq.com>
2017-05-27 15:11:37 +0800

    you can send file or image to the server in your webchat.last time i had learned how to send file to server according form submit,this i learn it by ajax submit file

--- my_bbs/webchat/urls.py
+++ my_bbs/webchat/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'^$', views.chat_dashboard),
     url(r'^msg_send/$', views.send_msg, name="send_msg"),  # url不写动词
     url(r'^new_msgs/$', views.get_new_msgs, name="get_new_msgs"),  # 取消息
-    url(r'^file_upload/$', views.webchat_file_upload, name="webchat_file_upload"),  # 取消息
+    url(r'^file_upload/$', views.webchat_file_upload, name="webchat_file_upload"),  # 上传文件(图片)
 
 ]
